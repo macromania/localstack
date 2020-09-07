07b701e295992102eb91ff970b8117ceeb4293ef
Waldemar Hummer <waldemar.hummer@gmail.com>
2020-09-08 00:54:08 +0200

    minor

--- localstack/utils/bootstrap.py
+++ localstack/utils/bootstrap.py
@@ -318,7 +318,7 @@ class PortMappings(object):
             for i in range(port[1] - port[0] + 1):
                 self.add(port[0] + i, mapped[0] + i)
             return
-        if int(port) <= 0:
+        if port is None or int(port) <= 0:
             raise Exception('Unable to add mapping for invalid port: %s' % port)
         if self.contains(port):
             return
